49bde080f671c2d1febdcc7e52d065fefb4df11c
ArseneNgenzi <arsenengenzi@gmail.com>
2024-02-06 09:04:32 +0200

    bug fix

--- app/dashboard/app-users/page.tsx
+++ app/dashboard/app-users/page.tsx
@@ -1,12 +1,10 @@
-import Accordion from '@/components/accordion/Accordion'
 import React from 'react'
 
 const AppUsers = () => {
   return (
-    <>
-    <div className=' h- flex justify-center items-center'>AppUsers</div>
-    <Accordion/>
-    </>
+
+    <div className=' h-[80svh] flex justify-center items-center'>AppUsers</div>
+
   )
 }
 
